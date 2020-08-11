0ee49f6e28da7f15c99c85bcdfa7e3bc4462e082
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-08-11 11:39:38 +0200

    Update SettingsAndSchedule.xaml.cs to connect checkbox RunOnStartUp to StartupManager.cs
    Added functionality to EnableRunOnStartup checkbox. The checkbox will enable and disable the program running on Windows as the user desires.

--- ToggleWindowsDarkMode/ToggleDarkMode/WPF-Directory/SettingsAndSchedule.xaml.cs
+++ ToggleWindowsDarkMode/ToggleDarkMode/WPF-Directory/SettingsAndSchedule.xaml.cs
@@ -22,6 +22,25 @@ namespace ToggleWindowsDarkMode
         public SettingsAndSchedule()
         {
             InitializeComponent();
+
+            if (StartupManager.StartupState == Enums.StartupState.Enabled)
+            {
+                EnableRunOnStartup.IsChecked = true;
+            }
+            else
+            {
+                EnableRunOnStartup.IsChecked = false;
+            }
+        }
+
+        private void EnableRunOnStartup_Checked(object sender, RoutedEventArgs e)
+        {
+            StartupManager.StartupState = Enums.StartupState.Enabled;
+        }
+
+        private void EnableRunOnStartup_Unchecked(object sender, RoutedEventArgs e)
+        {
+            StartupManager.StartupState = Enums.StartupState.Disabled;
         }
     }
 }
