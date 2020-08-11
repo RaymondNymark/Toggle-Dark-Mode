1412b876261278b845600e7e3ba491f8333f338a
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-08-11 11:33:09 +0200

    Create Enums.cs

---
+++ ToggleWindowsDarkMode/ToggleDarkMode/Core/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToggleWindowsDarkMode
+{
+    public static class Enums
+    {
+        public enum StartupState
+        {
+            Disabled,
+            DisabledByUser,
+            Enabled
+        }
+
+    }
+}
