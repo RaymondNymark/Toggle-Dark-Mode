deba7bd87da44b6085940ed208654add2c762950
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-08-13 08:28:07 +0200

    Remove unused files and legacy methods

--- ToggleWindowsDarkMode/ToggleDarkMode/WPF-Directory/DarkModeUtilityViewModel.cs
+++ ToggleWindowsDarkMode/ToggleDarkMode/WPF-Directory/DarkModeUtilityViewModel.cs
@@ -22,7 +22,7 @@ namespace ToggleWindowsDarkMode
             }
         }
 
-
+        // Todo: implement ShowAboutCommand
         public ICommand ShowAboutCommand
         {
             get
@@ -50,40 +50,6 @@ namespace ToggleWindowsDarkMode
                         Application.Current.MainWindow = new SettingsAndSchedule();
                         Application.Current.MainWindow.Show();
                     }
-                };
-            }
-        }
-
-        /// <summary>
-        /// Shows a window, if none is already open.
-        /// </summary>
-        public ICommand ShowWindowCommand
-        {
-            get
-            {
-                return new DelegateCommand
-                {
-                    CanExecuteFunc = () => Application.Current.MainWindow == null,
-                    CommandAction = () =>
-                    {
-                        Application.Current.MainWindow = new MainWindow();
-                        Application.Current.MainWindow.Show();
-                    }
-                };
-            }
-        }
-
-        /// <summary>
-        /// Hides the main window. This command is only enabled if a window is open.
-        /// </summary>
-        public ICommand HideWindowCommand
-        {
-            get
-            {
-                return new DelegateCommand
-                {
-                    CommandAction = () => Application.Current.MainWindow.Close(),
-                    CanExecuteFunc = () => Application.Current.MainWindow != null
                 };
             }
         }
