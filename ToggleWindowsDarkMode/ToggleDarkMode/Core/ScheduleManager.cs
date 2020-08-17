45b8e1d446ec3521508ceb4658be6e0fde215b94
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-08-17 12:45:33 +0200

    Remove debug & legacy code from ScheduleManager.cs

--- ToggleWindowsDarkMode/ToggleDarkMode/Core/ScheduleManager.cs
+++ ToggleWindowsDarkMode/ToggleDarkMode/Core/ScheduleManager.cs
@@ -10,104 +10,6 @@ namespace ToggleWindowsDarkMode
     {
         private static CancellationTokenSource cancellationTokenSource;
 
-        public static void CancelScheduledTask()
-        {
-            cancellationTokenSource.Cancel();
-        }
-
-        /// <summary>
-        /// Method to run a task at a specific time.  For this application, a
-        /// certain task will be to toggle between dark mode at a certain time.
-        /// This task can be canceled. 
-        /// </summary>
-        /// <param name="WhenToRunTask">At what DateTime to run a specific
-        /// Task</param>
-        /// <param name="RepeatTask">Boolean, if set to true the task will
-        /// repeat running.</param>
-        /// <returns></returns>
-        public static async Task RunTaskAtSpecificTimeAsync123(DateTime WhenToRunTask, bool RepeatTask)
-        {
-            var TimeSpanInMS = (int)(WhenToRunTask - DateTime.UtcNow).TotalMilliseconds;
-
-            if (TimeSpanInMS < 0)
-            {
-                // Shoot off task immediately, and 
-                ToggleDarkMode.SwitchTheme();
-                if (RepeatTask)
-                {
-                    await RunTaskAtSpecificTimeAsync(WhenToRunTask.AddDays(1), true);
-                }
-            }
-            else
-            {
-                // Delay the running of task for set amount of time.
-                var cancellationTokenSource = new CancellationTokenSource();
-                {
-                    try
-                    {
-                        await Task.Delay(TimeSpanInMS).ContinueWith((x) =>
-                        {
-                            ToggleDarkMode.SwitchTheme();
-                        }, cancellationTokenSource.Token);
-                    }
-                    catch (TaskCanceledException)
-                    {
-                        // Don't really do anything.
-                    }
-                }
-            }
-        }
-        public static void RunTaskAtSpecificTime(DateTime whenToRunTask, bool repeatTask)
-        {
-            var cancellationTokenSource = new CancellationTokenSource();
-            var TimeSpanInMS = (int)(whenToRunTask - DateTime.UtcNow).TotalMilliseconds;
-
-
-        }
-
-        public static async Task RunTaskAtSpecificTimeStartupAsync()
-        {
-            // Encapsulate this somewhere else?
-            if (Properties.Settings.Default.ScheduleEnabled)
-            {
-                var savedStartTime = Properties.Settings.Default.ScheduledTime;
-                await RunTaskAtSpecificTimeAsync(savedStartTime, true);
-            }
-        }
-
-
-        public static void TestTask()
-        {
-            var ctSource = new CancellationTokenSource();
-
-            var tsinMS = 5000;
-
-            Task.Delay(tsinMS).ContinueWith((x) =>
-            {
-                ToggleDarkMode.SwitchTheme();
-
-                TestTask();
-            }, ctSource.Token);
-
-        }
-
-        /// <summary>
-        /// Runs SwitchTheme task after a delay in MS.
-        /// </summary>
-        /// <param name="howLongToDelayTaskForInMS">Delay in MS.</param>
-        /// <returns></returns>
-        private static Task RunTaskAfterDelay(int howLongToDelayTaskForInMS)
-        {
-            //cancellationTokenSource = new CancellationTokenSource();
-            return Task.Run(() =>
-            {
-                Task.Delay(howLongToDelayTaskForInMS).ContinueWith((x) =>
-                {
-                    if (!cancellationTokenSource.IsCancellationRequested) ToggleDarkMode.SwitchTheme();
-                });
-            });
-        }
-
 
         public static async Task RunTaskAtSpecificTimeAsync(DateTime whenToRunTask, bool repeatTask)
         {
@@ -126,33 +28,49 @@ namespace ToggleWindowsDarkMode
             }
             else
             {
-                // Delay running the task for a set amount of time.
-                await RunTaskAfterDelay(timeSpanInMS).ContinueWith(async (x)=> { if (repeatTask) await Task.Run(() => RunTaskAtSpecificTimeAsync(whenToRunTask.AddSeconds(5), true)); });
-
-                //if (repeatTask)
-                //{
-                //    await RunTaskAtSpecificTimeAsync(whenToRunTask.AddDays(1), true);
-                //}
+                // Delay running a task by a set number of time. After it ran,
+                // repeat it but make it run 24hr later.
+                await RunTaskAfterDelay(timeSpanInMS).ContinueWith(async (x) => { if (repeatTask) await Task.Run(() => RunTaskAtSpecificTimeAsync(whenToRunTask.AddSeconds(5), true)); });
             }
         }
 
 
-        public static async Task ExecuteTaskAsync()
+
+
+        /// <summary>
+        /// Runs SwitchTheme task after a delay in MS.
+        /// </summary>
+        /// <param name="howLongToDelayTaskForInMS">Delay in MS.</param>
+        /// <returns></returns>
+        private static Task RunTaskAfterDelay(int howLongToDelayTaskForInMS)
         {
-            cancellationTokenSource = new CancellationTokenSource();
-            using (cancellationTokenSource)
+            //cancellationTokenSource = new CancellationTokenSource();
+            return Task.Run(() =>
             {
-                try
+                Task.Delay(howLongToDelayTaskForInMS).ContinueWith((x) =>
                 {
-                    await RunTaskAfterDelay(5000);
-                }
-                catch (TaskCanceledException)
-                {
-                    //CanceledStatus.Content = "Canceled";
-                    // Dont do anything
-                }
-            }
+                    if (!cancellationTokenSource.IsCancellationRequested) ToggleDarkMode.SwitchTheme();
+                });
+            });
         }
 
+        
+
+
+        public static void CancelScheduledTask()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+
+        public static async Task RunTaskAtSpecificTimeStartupAsync()
+        {
+            // Encapsulate this somewhere else?
+            if (Properties.Settings.Default.ScheduleEnabled)
+            {
+                var savedStartTime = Properties.Settings.Default.ScheduledTime;
+                await RunTaskAtSpecificTimeAsync(savedStartTime, true);
+            }
+        }
     }
 }
