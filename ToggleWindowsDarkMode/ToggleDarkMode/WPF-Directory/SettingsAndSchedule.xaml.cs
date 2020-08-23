294c2f68eaab5d9977c608341982afe960b55880
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-08-23 08:22:26 +0200

    Tidy up SettingsAndSchedule.xaml.cs

--- ToggleWindowsDarkMode/ToggleDarkMode/WPF-Directory/SettingsAndSchedule.xaml.cs
+++ ToggleWindowsDarkMode/ToggleDarkMode/WPF-Directory/SettingsAndSchedule.xaml.cs
@@ -17,26 +17,18 @@ namespace ToggleWindowsDarkMode
             InitializeComponent();
 
             // TODO : Implement data binding to avoid this.
-
-            if (StartupManager.StartupState == Enums.StartupState.Enabled)
+            if (StartupManager.StartupState == Enums.StartupState.Enabled) EnableRunOnStartup.IsChecked = true;
+            else EnableRunOnStartup.IsChecked = false;
+            
+            if (ScheduleManager.ScheduleState == Enums.ScheduleState.Enabled) EnableScheduling.IsChecked = true;
+            else EnableScheduling.IsChecked = false;
+            
+            // Changes ScheduleNotice label to tell the user when next automatic
+            // theme switch is scheduled at.
+            if (Properties.Settings.Default.SchedulingIsEnabled) ScheduleNotice.Content = $"The next automated theme switch is at: {Properties.Settings.Default.ScheduledTime.ToLocalTime()}"; else
             {
-                EnableRunOnStartup.IsChecked = true;
+                ScheduleNotice.Content = "";
             }
-            else
-            {
-                EnableRunOnStartup.IsChecked = false;
-            }
-
-            if (ScheduleManager.ScheduleState == Enums.ScheduleState.Enabled)
-            {
-                EnableScheduling.IsChecked = true;
-            }
-            else
-            {
-                EnableScheduling.IsChecked = false;
-            }
-
-            //ScheduleTime.Value = Properties.Settings.Default.ScheduledTime;
         }
 
         private void EnableRunOnStartup_Checked(object sender, RoutedEventArgs e)
@@ -49,20 +41,6 @@ namespace ToggleWindowsDarkMode
             StartupManager.StartupState = Enums.StartupState.Disabled;
         }
 
-        #region Debug-Options and Methods
-        private void ScheduleRunButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Should toggle darkmode after 5 seconds.
-            //ScheduleManager.RunTaskAtSpecificTimeAsync(DateTime.UtcNow.AddSeconds(8), true);
-        }
-
-        private void ScheduleCancel_Click(object sender, RoutedEventArgs e)
-        {
-            ScheduleManager.CancelScheduledTask();
-        }
-
-        #endregion
-
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.MainWindow.Close();
@@ -74,8 +52,6 @@ namespace ToggleWindowsDarkMode
             SaveButton.IsEnabled = true;
 
             if (EnableScheduling.IsChecked != Properties.Settings.Default.SchedulingIsEnabled) SaveSettings.IsEnabled = true;
-            //ScheduleTime.IsEnabled = true;
-            //SaveSettings.IsEnabled = true;
         }
 
         private void EnableScheduling_Unchecked(object sender, RoutedEventArgs e)
@@ -100,6 +76,45 @@ namespace ToggleWindowsDarkMode
             SaveSettings.IsEnabled = false;
         }
 
+        private void SaveButtonV2_Click(object sender, RoutedEventArgs e)
+        {
+            if (ScheduleManager.cancellationTokenSource.Token.CanBeCanceled)
+            {
+                ScheduleManager.cancellationTokenSource.Cancel();
+            }
+
+            var scheduleInput = ((DateTime)ScheduleTime.Value).ToUniversalTime();
+
+            // Checks if any of the inputs are different, if they are it enables
+            // the main save button.
+            if (scheduleInput != Properties.Settings.Default.ScheduledTime || EnableScheduling.IsChecked != Properties.Settings.Default.SchedulingIsEnabled) SaveSettings.IsEnabled = true;
+
+            Properties.Settings.Default.ScheduledTime = scheduleInput;
+            Properties.Settings.Default.Save();
+        }
+
+        // Hyperlink event handler.
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+
+        // Bellow here are only debug methods. Debug tab is disabled by default.
+        // Mostly related to testing ScheduleManager.
+        #region Debug-Options and Methods
+
+        private void ScheduleRunButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Should toggle darkmode after 5 seconds.
+            //ScheduleManager.RunTaskAtSpecificTimeAsync(DateTime.UtcNow.AddSeconds(8), true);
+        }
+
+        private void ScheduleCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ScheduleManager.CancelScheduledTask();
+        }
         private void DebugScheduleStart_Click(object sender, RoutedEventArgs e)
         {
             DateTime time = (DateTime)DebugScheduleTimePicker.Value;
@@ -116,34 +131,6 @@ namespace ToggleWindowsDarkMode
             ToggleDarkMode.SwitchTheme();
         }
 
-        private void ClearScheduleButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void SaveButtonV2_Click(object sender, RoutedEventArgs e)
-        {
-            if (ScheduleManager.cancellationTokenSource.Token.CanBeCanceled)
-            {
-                ScheduleManager.cancellationTokenSource.Cancel();
-            }
-
-            var scheduleInput = ((DateTime)ScheduleTime.Value).ToUniversalTime();
-
-            // Checks if any of the inputs are different, if they are it enables
-            // the main save button.
-            if (scheduleInput != Properties.Settings.Default.ScheduledTime || EnableScheduling.IsChecked != Properties.Settings.Default.SchedulingIsEnabled) SaveSettings.IsEnabled = true;
-            //if (scheduleInput != Properties.Settings.Default.ScheduledTime) SaveSettings.IsEnabled = true;
-
-
-            Properties.Settings.Default.ScheduledTime = scheduleInput;
-            Properties.Settings.Default.Save();
-        }
-
-        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
-            e.Handled = true;
-        }
+        #endregion
     }
 }
